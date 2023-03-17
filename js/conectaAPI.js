8808e8b95f1588259f8d9a223af94552ddb8b857
Sandro <sandromedeirosdacunha@gmail.com>
2023-03-17 14:12:22 -0300

    Adicionando Lista por requisição GET e começando requisição POST para criar videos

---
+++ js/conectaAPI.js
@@ -0,0 +1,27 @@
+async function listaVideos(){
+    const conexao = await fetch("http://localhost:3000/videos")
+    const conexaoJSON = await conexao.json()
+    return conexaoJSON
+}
+
+async function criaVideos(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações `,
+            url : url,
+            imagem: imagem
+        })
+    })
+
+    const conexaoJSON = await conexao.json()
+    return conexaoJSON
+}
+export const conectaAPI = {
+    listaVideos,
+    criaVideos
+}
